39ff98497f4cbc92239773850e92ff932f83ec27
Marcos-calisse <marcos.andres611@gmail.com>
2022-03-06 20:52:25 -0300

    Agregue funciones al proyecto

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
-const carritoDeCompras = (producto, stock, cantidadPedido) =>{
-    console.log(producto, stock, cantidadPedido)
+let pachatas = 600;
+let lomos = 800;
+let Hamburguesas = 400;
+
+
+// Funcion carrito de compras
+
+
+const carritoDeCompras = (producto, precio, cantidadPedido) =>{
+    
+    let resultado = precio * cantidadPedido;
+
+    console.log(`Elegiste ${cantidadPedido} ${producto} por un total de ${resultado}`)
 }
 
-carritoDeCompras("lomo", 10, 2);
 
+let nombreUsuario = prompt("Ingresa tu usuario");
+let contrasenia = prompt("Ingresa tu contraseña");
+let email = prompt("Ingresa tu emmail");
+
+validarUsuario();
+
+// Funcion login
+
+const validarUsuario = (nombreUsuario, contrasenia, email) => {
+    if (nombreUsuario === "Marcos" && contrasenia === 12345 && email === "mcalisse@gmail.com" ){
+        alert(`Bienvenido ${nombreUsuario}`)
+    }else{
+        alert(`Vuelve a intentarlo`)
+    }
+}
+
+carritoDeCompras("lomos", lomos, 2);
 
