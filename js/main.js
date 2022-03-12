b9306e7427fd51ffbbefdafadfee6291f69506fc
Marcos-calisse <marcos.andres611@gmail.com>
2022-03-12 17:53:06 -0300

    agregue array con productos y metodos

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ const carritoDeCompras = (producto, precio, cantidadPedido) =>{
     
     let resultado = precio * cantidadPedido;
 
-    //console.log(`Elegiste ${cantidadPedido} ${producto} por un total de ${resultado}`)
+    console.log(`Elegiste ${cantidadPedido} ${producto} por un total de ${resultado}`)
 }
 
 
@@ -20,8 +20,8 @@ let nombreUsuario = prompt("Ingresa tu usuario");
 let contrasenia = parseInt(prompt("Ingresa tu contraseña"));
 let email = prompt("Ingresa tu emmail");
 
-const validarUsuario = (nombreUsuario, contrasenia, email) => {
-    if (nombreUsuario == "Marcos" && contrasenia == 12345 && email == "mcalisse@gmail.com" ){
+const validarUsuario = (nombreIngresado, contrasenia, email) => {
+    if (nombreIngresado == "Marcos" && contrasenia == 12345 && email == "mcalisse@gmail.com" ){
         alert(`Bienvenido ${nombreUsuario}`)
     }else{
         alert(`Vuelve a intentarlo`)
@@ -30,17 +30,12 @@ const validarUsuario = (nombreUsuario, contrasenia, email) => {
 
 
 
-
-
-
-
-
 validarUsuario();
 
 carritoDeCompras("lomos", lomos, 2);
 
 
-// array de productos
+// Objeto constructor
 
 class Productos{
     constructor (id, nombre, precio, stock){
@@ -50,18 +45,41 @@ class Productos{
         this.stock = stock;
     }
     
-    
 }
 
 const nuevoProducto = new Productos ( 4,"papas fritas", 250, 10);
 
+
+// array de productos
+
+
 const listaProductos = [
-    {id: 1, nombre: "pachata", precio: 600, stock: 10},
-    {id: 2, nombre: "lomo", precio:800, stock: 10},
-    {id: 3, nombre: "hamburguesa", precio: 400, stock: 10}
+    {id: 1,
+     nombre: "pachata", 
+     precio: 600, 
+     stock: 10
+    },
+    {id: 2, 
+     nombre: "lomo", 
+     precio:800, 
+     stock: 10
+    },
+    {id: 3, 
+     nombre: "hamburguesa", 
+     precio: 400, 
+     stock: 10
+    }
 ];
 
-listaProductos.push(nuevoProducto);
 
+listaProductos.push({id: 4, nombre: "papas fritas", precio: 250, stock: 10}); // agrega objeto al array
+console.log(listaProductos);
+
+console.log(listaProductos[1].nombre); // devuelve el valor de la propiedad en la posicion del indice
+
+listaProductos.pop();                  // elimina el ultimo indice del array
+console.log(listaProductos);
+
+listaProductos.reverse();              // da vuelta el array
 console.log(listaProductos);
 
