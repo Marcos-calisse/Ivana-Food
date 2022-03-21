960528e86335f9b81889e4762e388916528819ef
Marcos-calisse <marcos.andres611@gmail.com>
2022-03-20 22:04:40 -0300

    Revisar inputs de Formulario Login

--- js/main.js
+++ js/main.js
@@ -3,68 +3,6 @@ let lomos = 800;
 let Hamburguesas = 400;
 const carrito = [];
 
-// Funcion carrito de compras --> llamo la funcion desde el html
-
-
-const carritoDeCompras = (producto, precio, cantidadPedido) =>{
-    
-    let resultado = precio * cantidadPedido;
-
-    console.log(`Elegiste ${cantidadPedido} ${producto} por un total de ${resultado}`)
-}
-
-
-// Funcion agregar porductos al carrito como lista de productos
-
-const agregarAlCarrito = (producto) =>{
-    carrito.push(producto)
-}
-
-
-// Funcion login
-
-// let nombreUsuario = prompt("Ingresa tu usuario");
-// let contrasenia = parseInt(prompt("Ingresa tu contraseña"));
-// let email = prompt("Ingresa tu emmail");
-
-const validarUsuario = (nombreIngresado, contrasenia, email) => {
-    if (nombreIngresado == "Marcos" && contrasenia == 12345 && email == "mcalisse@gmail.com" ){
-        alert(`Bienvenido ${nombreUsuario}`)
-    }else{
-        alert(`Vuelve a intentarlo`)
-    }
-}
-
-//validarUsuario(nombreUsuario, contrasenia, email);
-
-
-// Funcion genedora de cards
-
-function generarCards(productos){
-    let acumuladorDeCards = ``;
-    productos.forEach((producto)=>{
-        acumuladorDeCards += `<div class="card" style="width: 18rem;">
-        <img src="${producto.imagen}" class="card-img-top" alt="...">
-        <div class="card-body">
-        <h5 class="card-title">${producto.nombre}</h5>
-        <p class="card-text">$ ${producto.precio}</p>
-          <a href="#" class="btn btn-primary">Comprar</a>
-        </div>
-      </div>`;
-    });
-    imprimirCardsEnHTML(acumuladorDeCards)
-}
-
-// Funcion mostrar cards
-
-function imprimirCardsEnHTML(cards){
-    document.getElementById("cards").innerHTML = cards ;
-}
-
-function buscadorProductos () {
-    const productoEncontrado = document.getElementById("input").value.toLowercase();
-    
-}
 // Objeto constructor
 
 // class Productos{
@@ -76,8 +14,6 @@ function buscadorProductos () {
 //     }
     
 // }
-
-
 
 // array de productos
 
@@ -100,9 +36,101 @@ const listaProductos = [
      precio: 400, 
      stock: 10,
      imagen: "/multimedia/hamburguesa.jpeg"
+    },
+    {id: 4, 
+     nombre: "Papas fritas", 
+     precio: 250, 
+     stock: 10,
+     imagen: "http://c.files.bbci.co.uk/104B4/production/_103104766_gettyimages-957724442.jpg"
     }
 ];
 
+// Funcion carrito de compras --> llamo la funcion desde el html
+
+
+const carritoDeCompras = () =>{
+    const totalDelCarrito = listaProductos.reduce((acumulador, producto) => acumulador + producto.precio, 0);
+    return totalDelCarrito;
+}
+
+
+// Funcion agregar porductos al carrito como lista de productos
+
+const agregarAlCarrito = (producto) =>{
+    carrito.push(producto)
+}
+
+// Funcion genedora de cards
+
+function generarCards(productos){
+    let acumuladorDeCards = ``;
+    productos.forEach((producto)=>{
+        acumuladorDeCards += `<div class="card" style="width: 18rem;">
+        <img src="${producto.imagen}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${producto.nombre}</h5>
+        <p class="card-text">$ ${producto.precio}</p>
+          <button class="btn btn-primary" onclick="agregarAlCarrito(listaProductos)">Comprar</button>
+        </div>
+      </div>`;
+    });
+    imprimirCardsEnHTML(acumuladorDeCards)
+}
+
+// Funcion mostrar cards
+
+function imprimirCardsEnHTML(cards){
+    document.getElementById("cards").innerHTML = cards ;
+}
 
 generarCards(listaProductos);
+
+
+// Funcion login
+
+function generarFormLogin (){
+    const datosUsuario = document.getElementById("login");
+    datosUsuario.innerHTML = `<div class="contact__secction-container">
+    <div class="row">
+      <div class="contact__secction__item">
+        <label>Nombre Usuario</label>
+        <input type="text" id="nombre" placeholder="Ingresa tu usuario"/>
+      </div>
+      <div class="contact__secction__item">
+        <label>E-mail</label>
+        <input type="text" id="mail" placeholder="E-mail"/>
+      </div>
+      <div class="contact__secction__item">
+        <label>Contraseña</label>
+        <input type="password" id="contraseña" placeholder="Contraseña"/>
+      </div>
+      <div class="contact-button">
+        <button type="button" class="btn btn-danger" onclick = "validarUsuario()">Ingresar</button>
+      </div>
+    </div>
+  </div>`;
+}
+
+
+
+const validarUsuario = () => {
+
+    const nombreIngresado = document.getElementById("nombre").value;
+    const email = document.getElementById("mail").value.innerText;
+    const contrasenia = document.getElementById("contraseña").value;
+    
+    if (nombreIngresado === "Marcos" && contrasenia === 12345 && email === "mcalisse@gmail.com" ){
+
+        alert(`Bienvenido ${nombreIngresado}`)
+
+    }else{
+        alert(`Vuelve a intentarlo`)
+    }
+}
+
+
+
+
+
+
 
