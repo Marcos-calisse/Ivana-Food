973cbd7ad4661c5c419b61414ad1917a421ecde8
Marcos-calisse <marcos.andres611@gmail.com>
2022-05-09 18:50:42 -0300

    correccion final final

--- js/cartLogic.js
+++ js/cartLogic.js
@@ -1,4 +1,3 @@
 const storageCarrito = JSON.parse(localStorage.getItem("carrito")) || [];
 let carrito = storageCarrito;
 document.getElementById("cantidad-producto").innerHTML = carrito.length;
-console.log(carrito)
